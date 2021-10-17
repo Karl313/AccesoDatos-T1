5cad4a5160d72785daea06da597b8c10eae41809
Karl313 <carlos.bermejo@users.noreply.github.com>
2021-10-17 13:51:00 +0200

    ejercicio2.123

--- src/ejercicio2/Ejercicio22.java
+++ src/ejercicio2/Ejercicio22.java
@@ -2,19 +2,29 @@ package Ejercicio2;
 
 import java.io.File;
 
-/**
- * @author CFGS
- */
+
+
 public class Ejercicio22 {
-
+   
     public static void main(String[] args) {
-String fichero = "fichero.txt";
-String ruta = new File(fichero).getAbsolutePath();
-         
-    }//fin del main
+        
+    
+    File[] files = new File("C:\\Users\\carlo\\Documents\\NetBeansProjects"
+            + "\\Ejercicio2\\src\\ejercicio2\\Ejercicio21.java").listFiles();
+    if (files != null) 
+       Dictador(files);
+}
 
-    private static void ficheroDirectorio() {
-      
+public static void Dictador(File[] files) {
+    for (File file : files) 
+        
+        if (file.isDirectory()) {
+            Dictador(file.listFiles());
+             System.out.println("archivo----> " + file.getName());
+        }  else if(file.isFile()) {
+            System.out.println("Carpeta------>"+file.getName());
+        }
     }
 
-}// fin de la clase
+}
+
